d965bcb31032a6681ab1cc80cf7114d114e7e7d2
Alex Wulf <aw@dc.ag>
2022-11-21 13:06:01 +0100

    FGQN simplified

--- src/Domain/Hosting/Models/Project.php
+++ src/Domain/Hosting/Models/Project.php
@@ -6,12 +6,13 @@ use Domain\Users\Models\User;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory, HasUuids;
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
